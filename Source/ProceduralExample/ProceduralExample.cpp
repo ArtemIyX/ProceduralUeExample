ae03d7a290f084a3dc9adac1bc973b9d460c9076
Wellsaik <artem.podorozhko@outlook.com>
2024-08-12 15:49:50 +0300

    first commit

---
+++ Source/ProceduralExample/ProceduralExample.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProceduralExample.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProceduralExample, "ProceduralExample" );
